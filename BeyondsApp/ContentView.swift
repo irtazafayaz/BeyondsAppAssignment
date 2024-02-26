d2ea6be0243cd991a4471d97e2d942ca6b7c0370
Irtaza Fiaz <irtazab135@gmail.com>
2024-02-26 16:03:16 +0500

    -- Content View

--- BeyondsApp/ContentView.swift
+++ BeyondsApp/ContentView.swift
@@ -11,15 +11,37 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            NavigationLink("Image Filters") {
-                PresetsView()
-            }
-            NavigationLink("Collage") {
-                ImagePickerView()
-            }
-            NavigationLink("Whatsapp Status Screen") {
-                ImageEditingView()
-            }
+            
+            NavigationLink(destination: PresetsView(), label: {
+                Text("Question 1 - Image Filters")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+
+            })
+            
+            NavigationLink(destination: ImagePickerView(), label: {
+                Text("Question 2 - Collage")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.pink)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            })
+            .padding(.top, 20)
+            
+            NavigationLink(destination: ImageEditingView(), label: {
+                Text("Question 3 - Whatsapp Status")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            })
+            .padding(.top, 20)
+            
         }
     }
 }
